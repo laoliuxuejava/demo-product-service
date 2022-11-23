2e31e8070c299fd2c0104e80fe40b31fcd61a9f8
hand_cheng.xing <cheng.xing@hand-china.com>
2022-11-23 17:55:07 +0800

    上传数据文件

---
+++ demo-product-service.sql
@@ -0,0 +1,20 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 80029 (8.0.29)
+ Source Host           : localhost:3306
+ Source Schema         : demo-product-service
+
+ Target Server Type    : MySQL
+ Target Server Version : 80029 (8.0.29)
+ File Encoding         : 65001
+
+ Date: 23/11/2022 17:47:19
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+SET FOREIGN_KEY_CHECKS = 1;
